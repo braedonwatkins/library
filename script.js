a19b1afaa23847418becffa5f12ab05b416d40cc
BraedonWatkins <watkins.braedon@gmail.com>
2022-07-21 09:05:12 -0400

    updated addBook...()

--- script.js
+++ script.js
@@ -10,18 +10,32 @@ function Book(author, title, pages, readStatus) {
     this.readStatus = readStatus;
 }
 
-function addBookToLibrary(book) { myLibrary.push(book); }
+function addBookToLibrary() { 
+    let book = new Book();
+    book.title = document.getElementById("input-title");
+    book.author = document.getElementById("input-author");
+    book.pages = document.getElementById("input-pages");
+    book.readStatus = document.getElementById("input-status");
+
+    myLibrary.push(book); 
+
+    //testing
+    displayBookList();
+}
 
 
 // Testing
-let book1 = new Book("Holly Black", "The Wicked King", 180, false);
-let book2 = new Book("Jandy Nelson", "I'll Give You the Sun", 150, false);
-let book3 = new Book("J.K. Dipshit", "Harry Potter and the Goblet of Fire", 218, true);
+// let book1 = new Book("Holly Black", "The Wicked King", 180, false);
+// let book2 = new Book("Jandy Nelson", "I'll Give You the Sun", 150, false);
+// let book3 = new Book("J.K. Dipshit", "Harry Potter and the Goblet of Fire", 218, true);
 
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-addBookToLibrary(book3);
+// addBookToLibrary(book1);
+// addBookToLibrary(book2);
+// addBookToLibrary(book3);
+
+
 
 function displayBookList() { 
+
     myLibrary.forEach((book) => {console.table(book);}) 
 }
